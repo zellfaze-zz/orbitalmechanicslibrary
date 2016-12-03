a2fceca18fa11728cb4291ad4d08375d50d49e0f
Derric Atzrott <zellfaze@zellfaze.org>
2016-12-03 15:16:42 -0500

    Cleaned up main

--- main.cpp
+++ main.cpp
@@ -5,18 +5,5 @@
 #include "Orbit3D.h"
 
 int main() {
-  Vector3D v = Vector3D(120,90,45);
-  Vector3D v2 = Vector3D(7,15,90);
-
-  //std::cout << v.findAngle(v2).degrees() << std::endl;
-
-  Orbit3D orbit = Orbit3D();
-
-  orbit.e(0.5);
-  orbit.M(0.471239);
-
-  std::cout << "E: " << orbit.E().degrees() << std::endl;
-  std::cout << "f: " << orbit.f().degrees() << std::endl;
-
   return 0;
 }
